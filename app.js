c1facfaa97a2dc3bfb963901456989a36e43794c
Alex Morega <alex@grep.ro>
2015-10-01 22:24:12 +0300

    react component; render to html then inject

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 (function() {
 
+class Menu extends React.Component {
+
+  render() {
+    return (
+      <div className="demo-avatar-dropdown">
+        <span>hello@example.com</span>
+        <div className="mdl-layout-spacer"></div>
+        <button id="accbtn" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+          <i className="material-icons" role="presentation">arrow_drop_down</i>
+          <span className="visuallyhidden">Accounts</span>
+        </button>
+        <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" htmlFor="accbtn">
+          <li className="mdl-menu__item">hello@example.com</li>
+          <li className="mdl-menu__item">info@example.com</li>
+          <li className="mdl-menu__item">
+            <i className="material-icons">add</i>
+            Add another account...
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
+}
+
 var box = document.querySelector('#here')
-
-box.innerHTML =
-'<div class="demo-avatar-dropdown">\n'+
-'  <span>hello@example.com</span>\n'+
-'  <div class="mdl-layout-spacer"></div>\n'+
-'  <button id="accbtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">\n'+
-'    <i class="material-icons" role="presentation">arrow_drop_down</i>\n'+
-'    <span class="visuallyhidden">Accounts</span>\n'+
-'  </button>\n'+
-'  <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">\n'+
-'    <li class="mdl-menu__item">hello@example.com</li>\n'+
-'    <li class="mdl-menu__item">info@example.com</li>\n'+
-'    <li class="mdl-menu__item"><i class="material-icons">add</i>Add another account...</li>\n'+
-'  </ul>\n'+
-'</div>\n'
-
+box.innerHTML = React.renderToStaticMarkup(<Menu />)
 componentHandler.upgradeDom()
 
 })();
